504718081f9f99c4416ae1403ed8d24eae53ff27
shoriful hoque nobin <128087926+shoriful12win@users.noreply.github.com>
2026-01-22 17:01:55 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,95 @@
+const url = "https://api.github.com/users";
+const searchInputEl = document.getElementById("searchInput");
+const searchButtonEl = document.getElementById("searchBtn");
+const profileContainerEl = document.getElementById("profileContainer");
+const loadingEl = document.getElementById("loading");
+
+
+const generateProfile = (profile) => {
+  return `
+   <div class="profile-box">
+     <div class="top-section">
+       <div class="left">
+         <div class="avatar">
+           <img alt="avatar" src="${profile.avatar_url}" />
+         </div>
+         <div class="self">
+           <h1>${profile.name || profile.login}</h1> <h1>@${profile.login}</h1>
+         </div>
+       </div>
+       <a href="${profile.html_url}" target="_blank">
+         <button class="primary-btn">Check Profile</button>
+       </a>
+     </div>
+
+     <div class="about">
+       <h2>About</h2>
+       <p>
+         ${profile.bio ? profile.bio : "No Bio Available"} </p>
+     </div>
+     <div class="status">
+       <div class="status-item">
+         <h3>Followers</h3>
+         <p>${profile.followers}</p>
+       </div>
+       <div class="status-item">
+         <h3>Followings</h3>
+         <p>${profile.following}</p>
+       </div>
+       <div class="status-item">
+         <h3>Repos</h3>
+         <p>${profile.public_repos}</p>
+       </div>
+     </div>
+   </div>
+   `;
+};
+
+
+const fetchProfile = async () => {
+  const username = searchInputEl.value;
+
+
+  if (!username) {
+    loadingEl.innerText = "Please enter a username";
+    loadingEl.style.color = "red";
+    profileContainerEl.innerHTML = "";
+    return;
+  }
+
+  loadingEl.innerText = "loading.....";
+  loadingEl.style.color = "black";
+  profileContainerEl.innerHTML = ""; 
+
+  try {
+    const res = await fetch(`${url}/${username}`);
+    const data = await res.json();
+
+    
+    if (data.login) {
+      loadingEl.innerText = "";
+      profileContainerEl.innerHTML = generateProfile(data);
+    } else {
+      
+      loadingEl.innerText = data.message;
+      loadingEl.style.color = "red";
+      profileContainerEl.innerText = "";
+    }
+
+    console.log("data", data);
+  } catch (error) {
+    console.log({ error });
+    loadingEl.innerText = "Something went wrong!";
+    loadingEl.style.color = "red";
+  }
+};
+
+
+searchButtonEl.addEventListener("click", fetchProfile);
+
+
+searchInputEl.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    fetchProfile();
+  }
+});
